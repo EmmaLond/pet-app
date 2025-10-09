dd420203d6848e24f9fd5e99dc5bf0a382e13d7b
EmmaLond <147564243+EmmaLond@users.noreply.github.com>
2025-10-08 20:42:39 -0400

    Added values to main and example welcome text

--- app/src/main/java/com/example/petapp/MainActivity.kt
+++ app/src/main/java/com/example/petapp/MainActivity.kt
@@ -5,12 +5,28 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import android.widget.Toast
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val usernameText = findViewById<TextView>(R.id.userText)
+        val petsList = findViewById<RecyclerView>(R.id.recycleViewPets)
+        val addButton = findViewById<FloatingActionButton>(R.id.addPetButton)
+
+        //example static info to display on app
+        usernameText.text = "Hello, Emma!"
+        addButton.setOnClickListener {
+            Toast.makeText(this, "Add a new pet!", Toast.LENGTH_SHORT).show()
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
