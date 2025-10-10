db9b26e51bbd7c078fd0b79a28a44274bb56e3a9
Emilio Bosma <csafety246@gmail.com>
2025-10-10 12:26:06 -0400

    The Grind

---
+++ app/src/main/java/com/example/petapp/LogSign.kt
@@ -0,0 +1,27 @@
+package com.example.petapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class LogSign : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_log_sign)
+
+        val signUp = findViewById<Button>(R.id.signup_button)
+        val login = findViewById<Button>(R.id.login_button)
+
+        login.setOnClickListener {
+            val intent = Intent(this, login::class.java)
+            startActivity(intent)
+        }
+        signUp.setOnClickListener {
+            val intent = Intent(this, signUp::class.java)
+            startActivity(intent)
+        }
+    }
+}
