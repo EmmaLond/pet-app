d9953c8a165f7cee7bb9cf178ff21d786225b5e9
Emilio Bosma <csafety246@gmail.com>
2025-11-29 17:22:03 -0500

    Cleaned up imports
    Removed all unused imports from the activity files

--- app/src/main/java/com/example/petapp/PetProfile.kt
+++ app/src/main/java/com/example/petapp/PetProfile.kt
@@ -1,6 +1,5 @@
 package com.example.petapp
 
-import android.R.attr.name
 import android.content.Intent
 import android.os.Bundle
 import android.view.MenuInflater
--- app/src/main/java/com/example/petapp/Signup.kt
+++ app/src/main/java/com/example/petapp/Signup.kt
@@ -1,7 +1,6 @@
 package com.example.petapp
 
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
