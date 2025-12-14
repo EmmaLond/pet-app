961bf88d122513486ccfcd9c4da2db26da24d8bd
Emilio Bosma <csafety246@gmail.com>
2025-12-14 10:13:59 -0500

    Cleaning up sign up and putting password min

--- app/src/main/java/com/example/petapp/Signup.kt
+++ app/src/main/java/com/example/petapp/Signup.kt
@@ -2,6 +2,7 @@ package com.example.petapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils.isEmpty
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
@@ -30,11 +31,12 @@ class Signup : AppCompatActivity() {
         }
 
         signupButton.setOnClickListener {
-            // Set errors
-            var isEmpty = false
             val passwordLayout1 = findViewById<TextInputLayout>(R.id.show_password)
             val passwordLayout2 = findViewById<TextInputLayout>(R.id.show_password_again)
             val emailLayout = findViewById<TextInputLayout>(R.id.emailWrap)
+
+            // Check for empty values
+            var isEmpty = false
             if (email.text.isEmpty()) {
                 isEmpty = true
                 emailLayout.error = "Email is required."
@@ -53,6 +55,22 @@ class Signup : AppCompatActivity() {
 
             val passwordOne = password_one.text.toString().trim()
             val passwordTwo = password_two.text.toString().trim()
+            val userEmail = email.text.toString().trim()
+
+            // Verify content of text
+            if (passwordOne.length < 7) {
+                passwordLayout1.error = "Password is too short. Please have at least 7 characters."
+                return@setOnClickListener
+            }
+            if (passwordOne != passwordTwo) {
+                passwordLayout1.error = "Passwords do not match."
+                passwordLayout2.error = "Passwords do not match."
+                return@setOnClickListener
+            }
+            if (!userEmail.contains('@') || !userEmail.contains('.')) {
+                emailLayout.error = "Please enter a valid email."
+                return@setOnClickListener
+            }
 
             EspressoIdlingResource.signUpValidation.increment()
             lifecycleScope.launch {
@@ -60,17 +78,13 @@ class Signup : AppCompatActivity() {
                 val accountExist =
                     accountTable.getPassword(email = email.text.toString().trim())
 
-                if (passwordOne == passwordTwo && accountExist == null) {
+                if (accountExist == null) {
                     val intent = Intent(this@Signup, OTP::class.java)
                     intent.putExtra("email", email.text.toString().trim())
                     intent.putExtra("password", passwordOne)
                     startActivity(intent)
-                // Sets Errors
-                } else if (passwordOne != passwordTwo) {
-                    passwordLayout1.error = "Passwords do not match"
-                    passwordLayout2.error = "Passwords do not match"
                 } else {
-                    emailLayout.error = "An account with that email already exists"
+                    emailLayout.error = "An account with that email already exists."
                 }
                 EspressoIdlingResource.signUpValidation.decrement()
             }
